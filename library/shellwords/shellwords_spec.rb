61b8f7a162b06f7a77d77605e97077c9dd5d4aac
jbreeden <jared.breeden@gmail.com>
2016-03-06 13:38:46 -0800

    Mruby doesn't handle top level includes correctly

--- library/shellwords/shellwords_spec.rb
+++ library/shellwords/shellwords_spec.rb
@@ -1,31 +1,36 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 require 'shellwords'
 
-# TODO: Not working for MRuby
-# include Shellwords
+if RUBY_NAME != 'mruby'
+  include Shellwords
+else
+  def shellwords(*args)
+    Shellwords.shellwords(*args)
+  end
+end
 
 describe "Shellwords#shellwords" do
   it "honors quoted strings" do
-    Shellwords.shellwords('a "b b" a').should == ['a', 'b b', 'a']
+    shellwords('a "b b" a').should == ['a', 'b b', 'a']
   end
 
   it "honors escaped double quotes" do
-    Shellwords.shellwords('a "\"b\" c" d').should == ['a', '"b" c', 'd']
+    shellwords('a "\"b\" c" d').should == ['a', '"b" c', 'd']
   end
 
   it "honors escaped single quotes" do
-    Shellwords.shellwords("a \"'b' c\" d").should == ['a', "'b' c", 'd']
+    shellwords("a \"'b' c\" d").should == ['a', "'b' c", 'd']
   end
 
   it "honors escaped spaces" do
-    Shellwords.shellwords('a b\ c d').should == ['a', 'b c', 'd']
+    shellwords('a b\ c d').should == ['a', 'b c', 'd']
   end
 
   it "raises ArgumentError when double quoted strings are misquoted" do
-    lambda { Shellwords.shellwords('a "b c d e') }.should raise_error(ArgumentError)
+    lambda { shellwords('a "b c d e') }.should raise_error(ArgumentError)
   end
 
   it "raises ArgumentError when single quoted strings are misquoted" do
-    lambda { Shellwords.shellwords("a 'b c d e") }.should raise_error(ArgumentError)
+    lambda { shellwords("a 'b c d e") }.should raise_error(ArgumentError)
   end
 end
